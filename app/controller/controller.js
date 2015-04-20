497f9fe193a12cd8a737cb22fc3f86f3f1f3ceec
alexsimkovich <dand.was@gmail.com>
2015-04-20 12:47:42 +0300

    geoip

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -1,12 +1,15 @@
-weatherApp.controller('weatherController', function($scope, $http){
-    $scope.weathers = [];
-    $http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk').success(function(response) {
-        $scope.weathers = response;
-        console.log(response);
-        $scope.city = response.name;
-        /*$scope.temp_min = (response.main['temp_min'] - 32) * 5/9;*/
-        $scope.temp_min = Math.round(response.main['temp_min'] - 273.15);
-        $scope.temp_max = Math.round(response.main['temp_max'] - 273.15);
+weatherApp.controller('weatherController', function ($scope, $http, geoIP) {
+  $scope.weathers = [];
+  $scope.home = [];
 
+  geoIP.success(function(data) {
+    $scope.home = data;
+    $http.get('http://api.openweathermap.org/data/2.5/weather?q='+$scope.home.city+','+$scope.home.country_code+'').success(function (response) {
+      $scope.weathers = response;
+      //console.log(response);
+      $scope.city = response.name;
+      $scope.temp_min = Math.round(response.main['temp_min'] - 273.15);
+      $scope.temp_max = Math.round(response.main['temp_max'] - 273.15);
     });
+  });
 });
--- app/services/services.js
+++ app/services/services.js
@@ -1,7 +1,3 @@
-/*
-weatherApp.factory('Weather', ['$resource',
-    function($resource){
-        return $resource('api.openweathermap.org/data/2.5/weather?q=London,uk', {}, {
-            query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
-        });
-    }]);*/
+weatherApp.factory('geoIP', function ($http) {
+  return $http.get('https://freegeoip.net/json/');
+});
